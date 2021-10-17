46a69490f62f4c627df90826fc2f672699b17564
Abderrahim Farhat <abderrahimfarhat6@gmail.com>
2021-10-17 15:39:22 +0100

    add page home

--- Films/resources/views/home.blade.php
+++ Films/resources/views/home.blade.php
@@ -1,1 +1,232 @@
 @extends('layouts.app')
+@section('content')
+{{-- Crousel Images --}}
+<div class="mycontainer">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('images/image.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/image1.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+{{-- films and categories --}}
+<div class="mydiv">
+    <section>
+        <article class="Myarticles">
+            <div class="d-flex justify-content-between">
+                <h3>Action Films</h3>
+                <a class="" href="#">More &rarr;</a>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-3">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+        </article>
+        <article class="Myarticles">
+            <div class="d-flex justify-content-between">
+                <h3>Action Films</h3>
+                <a class="" href="#">More &rarr;</a>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-3">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+
+        </article>
+        <article class="Myarticles">
+            <div class="d-flex justify-content-between">
+                <h3>Action Films</h3>
+                <a class="" href="#">More &rarr;</a>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-3">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+
+        </article>
+        <article class="Myarticles">
+            <div class="d-flex justify-content-between">
+                <h3>Action Films</h3>
+                <a class="" href="#">More &rarr;</a>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-3">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 ">
+                    <div class="card" style="width: 15rem;">
+                        <img src="{{ asset('images/image1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+    </section>
+    <aside>
+        <h1>Categories</h1>
+            <div class="row">
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success  mybtn">2021 Films</button></div>
+                <div class="col-12 col-xl-6 "><button type="button" class="btn btn-outline-success mybtn">2020 Films</button></div>
+
+                <!-- Force next columns to break to new line -->
+                <div class="w-100"></div>
+
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Action Films</button></div>
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Horror Films</button></div>
+
+                <div class="w-100"></div>
+
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Comedy Films</button></div>
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Romance Films</button></div>
+
+                <div class="w-100"></div>
+
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Arabic Films</button></div>
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Korean Films</button></div>
+
+                <div class="w-100"></div>
+
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Anime Films</button></div>
+                <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-success mybtn">Cartoon Films</button></div>
+            </div>
+    </aside>
+</div>
+@endsection
--- Films/resources/views/layouts/app.blade.php
+++ Films/resources/views/layouts/app.blade.php
@@ -7,24 +7,38 @@
     <title>Document</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    {{-- FONT AWESOM --}}
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- My Style --}}
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <header>
-        <nav>
-            <p>A<span>BY</span>F<span>__</span>Films</p>
-            <div class="mynav">
-                <a href="home">Home</a>
-                <a href="home">Who Are We</a>
-                <a href="home">Contact Us</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid ">
+                <a class="navbar-brand" href="#"><p><span>A</span>BY<span>F</span>ilms</p></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class=" d-flex justify-content-start collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active"  href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Who We Are</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
-            <form action="">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
         </nav>
-        <hr>
     </header>
     @yield('content')
     <!-- JavaScript Bundle with Popper -->
