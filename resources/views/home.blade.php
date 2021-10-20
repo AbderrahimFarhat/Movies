a28e40c1ee278f217d311fc9aa33e8be16e3a8ff
unknown <youssefbahomman3@gmail.com>
2021-10-20 17:00:01 +0200

    updating home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
 @section('content')
-<div class="welcoming">
-    <a class="navbar-brand navBrand" href="#"><span>A</span>by<span>F</span>ilms</a>
-    <div class="title">
-        <h3><span>Wel</span>come <span>T</span>o <span>T</span>he <span>Ho</span>me <span>O</span>f <span>Mo</span>vies</h3>
+<div class="container">
+    <div class="welcoming">
+        <a class="logo" href="#"><span>A</span>by<span>F</span>ilms</a>
+        <div class="title">
+            <h3 class="logo"><span>Wel</span>come <span>T</span>o <span>T</span>he <span>Ho</span>me <span>O</span>f <span>Mo</span>vies</h3>
+        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-search" type="submit">Search</button>
+        </form>
     </div>
-    <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn btn-outline-danger" type="submit">Search</button>
-    </form>
 </div>
 {{-- Crousel Images --}}
 <div class="mycontainer">
@@ -34,118 +36,77 @@
         </button>
     </div>
 </div>
-{{--   and categories --}}
+{{--   movies --}}
 <div class="container">
-    <div class="mydiv">
-        <section>
-            <article class="Myarticles">
-                <div class="d-flex justify-content-between">
-                    <h3>Action   :</h3>
-                    <a class="" href="#">More &rarr;</a>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-xl-3">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-            </article>
-            <article class="Myarticles">
-                <div class="d-flex justify-content-between">
-                    <h3>Action   :</h3>
-                    <a class="" href="#">More &rarr;</a>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-xl-3">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-
-            </article>
-            <article class="Myarticles">
-                <div class="d-flex justify-content-between">
-                    <h3>Action   :</h3>
-                    <a class="" href="#">More &rarr;</a>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-xl-3">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-
-            </article>
-            <article class="Myarticles">
-                <div class="d-flex justify-content-between">
-                    <h3>Action   :</h3>
-                    <a class="" href="#">More &rarr;</a>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-xl-3">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                    <div class="col-12 col-xl-3 ">
-                        <x-movie-main-card />
-                    </div>
-                </article>
-
-        </section>
-        {{-- <aside>
-            <h4> More Categories</h4>
-                <div class="row">
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info  mybtn">2021  </button></div>
-                    <div class="col-12 col-xl-6 "><button type="button" class="btn btn-outline-info mybtn">2020  </button></div>
-
-                    <!-- Force next columns to break to new line -->
-                    <div class="w-100"></div>
-
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Action  </button></div>
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Horror  </button></div>
-
-                    <div class="w-100"></div>
-
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Comedy  </button></div>
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Romance  </button></div>
-
-                    <div class="w-100"></div>
-
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Arabic  </button></div>
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Korean  </button></div>
-
-                    <div class="w-100"></div>
-
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Anime  </button></div>
-                    <div class="col-12 col-xl-6"><button type="button" class="btn btn-outline-info mybtn">Cartoon  </button></div>
-                </div>
-        </aside> --}}
+    <div class="filter d-md-flex">
+        <div class="by-film-type flex-grow-1">
+            <h5 class="section-title">Advanced Filter</h5>
+            <div>
+                <select name="category">
+                    <option value="cateory" disabled selected>Category</option>
+                    <option value="action">Action</option>
+                    <option value="drama">Drama</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="sci-fi">Sci-fi</option>
+                </select>
+                <select name="year">
+                    <option value="year" disabled selected>Year</option>
+                    @for ($i = 0; $i < 22; $i++)
+                        <option value="{{2000+$i}}">{{2000 + $i}}</option>
+                    @endfor
+                </select>
+                <select name="Quality">
+                    <option value="quality" disabled selected>Quality</option>
+                    <option value="bluray">BluRay</option>
+                    <option value="webdl">WEB-DL</option>
+                    <option value="webrip">WEB-RIP</option>
+                    <option value="hdtv">HDTV</option>
+                </select>
+            </div>
+        </div>
+        <div class="by-public d-flex justify-content-around align-items-center flex-grow-1">
+            <a href="#">
+                <img src="icons/team.png" alt="">
+                <span>Most Views</span>
+            </a>
+            <a href="#">
+                <img src="icons/heart.png" alt="">
+                <span>Most Liked</span>
+            </a>
+            <a href="#">
+                <img src="icons/star.png" alt="">
+                <span>Most Rated</span>
+            </a>
+        </div>
+    </div>
+    <div class="movies">
+        <h5 class="section-title">Latest Movies</h5>
+        <div class="card-container">
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+            <x-movie-main-card />
+        </div>
+    </div>
+    <div class="pagination">
+        <ul>
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            
+        </ul>
     </div>
 </div>
+
 @endsection
