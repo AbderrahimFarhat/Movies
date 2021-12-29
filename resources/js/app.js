511eaf3630bc5a30ae2ad9c67cb446fc4f10ce44
BHMyoussef <youssefbahomman3@gmail.com>
2021-12-29 16:56:16 +0100

    adding search bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,36 @@
 require('./bootstrap');
-<<<<<<< HEAD
-
-
-=======
-function getxhr(){
-    try{xhr=new XMLHttpRequest();}
-    catch(e){
-       try{xhr=new ActiveXObject("Microsoft.XMLHTTP");}
-       catch(e1){
-          try{xhr=new ActiveXObject("Msxml2.XMLHTTTP");}
-          catch(e2){
-             alert("AJAX n'est pas supporté par votre navigateur!");
-          }
-       }
-    }
-    return xhr;
- }
 
  document.getElementById("search-input").addEventListener('keyup',(event)=>{
-   fetch("https://api.themoviedb.org/3/search/movie?api_key=f7811093155c51fea3e2df1050c0fe87&language=en-US&query=spiderman&page=1&include_adult=false")
+   keyword=event.target.value;
+   objs = document.getElementsByClassName("movie-search");
+   arrs = [...objs]
+   arrs.map((elm)=>{
+      elm.remove()
+   })
+   i=0;
+   fetch("https://api.themoviedb.org/3/search/movie?api_key=f7811093155c51fea3e2df1050c0fe87&language=en-US&query="+keyword+"&page=1&include_adult=false")
    .then(res=>res.json()).then(res=>{
-      console.log(res["results"])
+
+      res["results"].map(movie=>{
+         if(i<3){
+            i++;
+            a = document.createElement("a");
+            img = document.createElement("img");
+            p   = document.createElement("p");
+      
+            a.className="movie-search";
+            img.className="img-search";
+            p.className = "img-search-description"
+      
+            a.href = "/movie?title="+movie['title']+"&id="+movie['id'];
+            img.src ="https://image.tmdb.org/t/p/w300/"+ movie["poster_path"];
+            p.innerHTML=movie["overview"];
+   
+            a.appendChild(img);
+            a.appendChild(p);
+            document.getElementById("movie-search-holder").appendChild(a)
+         }
+
+      })
    })
 })
->>>>>>> searchComp
